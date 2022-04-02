4d48e763cc67bd3ea9fc3c81ab04996ec7a38387
k0oda <kulevgen32@gmail.com>
2022-04-03 03:07:30 +0600

    add project hour rate increase separation on billable/payable, fix invoices to matchn new system

--- wwwdj/models.py
+++ wwwdj/models.py
@@ -23,7 +23,8 @@ class User(AbstractUser):
 class Project(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    hour_rate_increase = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    payable_hour_rate_increase = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    billable_hour_rate_increase = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
 
     def __str__(self):
         return self.name
